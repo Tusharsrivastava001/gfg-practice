f47e2593d50d63a0e52ce3ee953c011ec9368f9e
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-06-26 16:31:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+      int firstpos(vector<int>&arr,int tar){
+        int start=0;
+        int ans=-1;
+        int end=arr.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==tar){
+                ans=mid;
+                end=mid-1;
+            }
+            else if(arr[mid]>tar){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    int lastpos(vector<int>&arr,int tar){
+        int start=0;
+        int ans=-1;
+        int end=arr.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==tar){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(arr[mid]>tar){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> find(vector<int>& nums, int target) {
+        // code here
+        return {firstpos(nums,target),lastpos(nums,target)};
+        
+    }
+};
